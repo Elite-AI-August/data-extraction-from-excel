4f95b5fdfca6f920059047776976f0bd4aca893f
CarollCheng <carollhwrd@gmail.com>
2018-08-06 16:09:32 +0200

    Extraction of data from the sheet and notebook for testing added

--- data_extractor.py
+++ data_extractor.py
@@ -5,8 +5,30 @@ import openpyxl
 #for country in countries:
 country="Austria"
 wb = openpyxl.load_workbook('C:\\Users\\Michael\\Downloads\\source_file_' + country + '.xlsx',read_only=True, data_only=True)
-sheets =  wb.get_sheet_names()
+sheets =  wb.sheetnames
+for i in range(0,3):
+    sheets.pop(i)
+sheets.pop()
 print(sheets)
+dates=[]
+costs=[]
+aff_group=[]
+country_name=[]
+data=[]
+sheet_name='Trovit_AT'
+sheet = wb[sheet_name]
+for row in range(3, 408):
+    datum = sheet['A' + str(row)].value
+    cost = sheet['G' + str(row)].value
+    aff=sheet['A502'].value
+    if datum != None:
+        dates.append(datum)
+        if cost !=None:
+            costs.append(cost)
+            aff_group.append(aff)
+            country_name.append(country)
+data=list(zip(dates,costs,aff_group,country_name))
+print(data)
 
 
 
