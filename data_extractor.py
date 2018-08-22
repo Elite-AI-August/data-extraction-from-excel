99fa0a140a7a599f484284f656f4e214a4b37e9d
CarollCheng <carollhwrd@gmail.com>
2018-08-22 16:00:34 +0200

    removed unused code+added exclusion of formula errors

--- data_extractor.py
+++ data_extractor.py
@@ -10,7 +10,7 @@ def refresh_cc_sheets(path,input_file,countries):
     Application = win32com.client.Dispatch("Excel.Application")
  
  # Show Excel. While this is not required, it can help with debugging
-    Application.Visible = 1
+    Application.Visible = 0
     Application.DisplayAlerts=False
     Application.AskToUpdateLinks = False
 
@@ -39,21 +39,13 @@ now=datetime.datetime.now()
 cur_date=now.strftime("%Y%m%d")
 #dictionary for iterating between files 
 countries={9:"Austria", 3:"Europe", 6:"France", 1:"Germany", 11:"Italy", 13:"Spain", 7:"Switzerland", 2:"UK", 12:"USA", 14:"Netherlands", 10:"Belgium"}
-#countries={9:"Austria"}
 path='Z:\\800-Management\\830-Controlling\\833-Marketing\\Channel Controlling 2018\\'
 input_file='Channel Controlling 2018 '
 print('Started at: {}'.format(now.strftime("%H:%M")))
+
 refresh_cc_sheets(path,input_file,countries)
 
-#path='C:\\Users\\Michael\\Downloads\\'
-#input_file='source_file_'
-#clearing the log file
-#with open(path + 'log'+ '.csv', 'w', newline='') as csvfile:
-#    filewriter = csv.writer(csvfile, delimiter=',',
-#                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#    filewriter.writerow(['Log date', now])
- #   filewriter.writerow(['Filename', 'Status'])
-#clearing the output file
+
 with open(path + cur_date +'_output'+ '.csv', 'w', newline='') as csvfile:
             filewriter = csv.writer(csvfile, delimiter=',',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -88,14 +80,14 @@ for c_id, country in countries.items():
         # extracting only cost and date
         for row in sheet.iter_rows(min_row=3,max_row=408, min_col=1, max_col=7):
             datum = row[0].value
-            cost = format_decimal(row[6].value, locale='de_DE')
+            cost = row[6].value
             #removing blank cells
             if datum == None:
                 continue
-            if cost !=None and cost != 0:
+            if cost not in [None,0,'#N/A','#VALUE!']:
                 #adding data to list
                 dates.append(datum.strftime("%Y-%m-%d"))
-                costs.append(cost)
+                costs.append(format_decimal(cost, locale='de_DE')) #formating numbers in German style
                 aff_group.append(sheet_name)
                 country_id.append(c_id)
         #putting lists together into a list of tuples 
@@ -113,10 +105,6 @@ for c_id, country in countries.items():
          #                                       quotechar='|', quoting=csv.QUOTE_MINIMAL)
          #   filewriter.writerow([sheet_name,'Done'])
 
-    #with open(path + 'log'+ '.csv', 'a+', newline='') as csvfile:
-    #    filewriter = csv.writer(csvfile, delimiter=',',
-    #                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #    filewriter.writerow([input_file + country,'Done'])
     print('{}{} Done Started at:{} Ended at:{}'.format(input_file,country,started,datetime.datetime.now().strftime("%H:%M")))
 
 
